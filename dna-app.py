5640df645ebc3352e5ed196aa23b323711db225f
Uvindu Thilanka <72906297+sciguysl@users.noreply.github.com>
2023-10-29 19:58:15 +0000

    Update dna-app.py

--- dna-app.py
+++ dna-app.py
@@ -11,10 +11,6 @@ from PIL import Image
 # Page Title
 ######################
 
-image = Image.open('dna-logo.jpg')
-
-st.image(image, use_column_width=True)
-
 st.write("""
 # Bacterial rRNA Modification Sites Finder
 
